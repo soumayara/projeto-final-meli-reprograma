fba90b27920662426cb97b3631c0195d84772daf
soumayara <soumayara@outlook.com>
2019-12-10 21:56:29 -0300

    inserido  dev: nodemon server.js_v2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require ("./src/app")
-const port = 3000
+//const port = 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, function() {
-    console.log(`app está rodando na porta ${port}`)
+app.listen(PORT, function() {
+    console.log(`app está rodando na porta ${PORT}`)
   })
